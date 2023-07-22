d566b4c82c760491df0f210d5af0bf64a5ce31f0
Zhou-Shilin <2839997522@qq.com>
2023-07-23 07:37:28 +0800

    新增对返回状态码的解释

--- request_sender.py
+++ request_sender.py
@@ -27,8 +27,10 @@ def sendRequest(api_url, apikey, uuid=None, remote_uuid=None, method="get", body
         response = requests.post(api_url, params=_params, headers=_headers, json=body)
     elif method == "put":
         response = requests.put(api_url, params=_params, headers=_headers, json=body)
+    elif method == "delete":
+        response = requests.delete(api_url, params=_params, headers=_headers, json=body)
     else:
-        raise NoThatMethod("没有这种请求方式（目前仅支持get, post, push）")
+        raise NoThatMethod("内部程序错误：没有这种请求方式（目前仅支持get, post, push, delete）")
 
     if response.status_code == 200:
         log("(sendRequest) Successful 200")
@@ -37,6 +39,13 @@ def sendRequest(api_url, apikey, uuid=None, remote_uuid=None, method="get", body
     else:
         log("(sendRequest) Failed " + str(response.status_code))
 
-        raise RequestException("请求返回状态码为" + str(response.status_code))
+        if response.status_code == 400:
+            reason = "参数错误"
+        elif response.status_code == 403:
+            reason = "无权限"
+        elif response.status_code == 500:
+            reason = "服务器异常"
+        
+        raise RequestException("请求返回状态码为" + str(response.status_code) + " " + reason)
 
         return False
